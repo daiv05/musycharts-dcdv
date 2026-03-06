23e70579172dadfcfa6c65d9cf63cf256f1250e9
daiv05 <davidderas50@gmail.com>
2026-03-06 13:32:47 -0600

    feat: update versions

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 import { VitePWA } from 'vite-plugin-pwa'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    tailwindcss(),
     VitePWA({
       includeAssets: ['fonts/*.ttf', 'images/*.png', "/favicon.ico"],
       manifest: {
